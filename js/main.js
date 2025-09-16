db7a3438322829388d9843b36db70d58a08ed5ac
Wanjivwa Sinkala <wanjivwasinkala89@gmail.com>
2025-09-16 14:25:16 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,22 +78,3 @@ window.addEventListener("scroll", () => {
 }
 */
 
-<script>
-  const backToTop = document.getElementById("back-to-top");
-
-  // Show button after scrolling 200px
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-      backToTop.style.display = "block";
-    } else {
-      backToTop.style.display = "none";
-    }
-  });
-
-  // Smooth scroll to top
-  backToTop.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-</script>
-
