3b9fd4afc3ac46eb025329446024ffa3ce956c71
Wanjivwa Sinkala <wanjivwasinkala89@gmail.com>
2025-09-17 15:41:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-<script>
-  window.addEventListener('DOMContentLoaded', () => {
-    const hero = document.querySelector('.fade-in');
-    if (hero) {
-      hero.classList.add('visible');
-    }
-  });
-</script>
-
 
